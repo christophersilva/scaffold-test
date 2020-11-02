bd5b09c018b56fb510db16e71888b76dff651063
Christopher Silva <silva.christopher96@gmail.com>
2020-11-02 17:29:55 -0300

    improv(Translation): add some translations

--- resources/lang/en/general.php
+++ resources/lang/en/general.php
@@ -4,7 +4,9 @@ return [
     'email' => 'Email',
     'password' => 'Password',
     'remember_me' => 'Remember me',
+    'already_registered' => 'Already registered?',
     'forgot_password' => 'Forgot your password?',
+    'confirm_password' => 'Confirm password',
     'register' => 'Register',
     'login' => 'Login',
     'videos' => 'Video|Videos',
@@ -17,5 +19,10 @@ return [
     'summary' => 'Content summary',
     'save' => 'Save',
     'discipline_create_success' => 'Discipline created with success!',
-    'discipline_update_success' => 'Discipline updated com success!',
+    'discipline_update_success' => 'Discipline updated with success!',
+    'video_create_success' => 'Video created with success!',
+    'video_update_success' => 'Video updated with success!',
+    'profile' => 'Profile',
+    'logout' => 'Logout',
+    'manage_account' => 'Manage Account'
 ];
