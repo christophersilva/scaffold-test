82250591c1f90827908506029e7c3e9067e412a2
Christopher Silva <silva.christopher96@gmail.com>
2020-11-02 06:36:17 -0300

    feat(Translations): add base translations

---
+++ resources/lang/en/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'email' => 'Email',
+    'password' => 'Password',
+    'remember_me' => 'Remember me',
+    'forgot_password' => 'Forgot your password?',
+    'register' => 'Register',
+    'login' => 'Login',
+    'videos' => 'Video|Videos',
+    'students' => 'Student|Students',
+    'disciplines' => 'Discipline|Disciplines',
+    'classes' => 'Class|Classes',
+    'new' => 'New',
+    'name' => 'Name',
+    'link' => 'Link',
+    'summary' => 'Content summary',
+    'save' => 'Save',
+    'discipline_create_success' => 'Discipline created with success!',
+    'discipline_update_success' => 'Discipline updated com success!',
+];
---
+++ resources/lang/pt_BR/general.php
@@ -0,0 +1,21 @@
+<?php
+
+return [
+    'email' => 'E-mail',
+    'password' => 'Senha',
+    'remember_me' => 'Lembrar senha?',
+    'forgot_password' => 'Esqueceu sua senha?',
+    'register' => 'Cadastrar',
+    'login' => 'Entrar',
+    'videos' => 'Video|Videos',
+    'students' => 'Aluno|Alunos',
+    'disciplines' => 'Disciplina|Disciplinas',
+    'classes' => 'Aula|Aulas',
+    'new' => 'Novo',
+    'name' => 'Nome',
+    'link' => 'Link',
+    'summary' => 'Resumo do conteúdo',
+    'save' => 'Salvar',
+    'discipline_create_success' => 'Disciplina criada com sucesso!',
+    'discipline_update_success' => 'Disciplina atualizada com sucesso!',
+];
